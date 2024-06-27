59070c66fda9a3d5bf0b9394947a87402ad95c1c
Angel <angel152265@gmail.com>
2024-06-27 17:47:48 -0500

    nuevos cambios

--- booking-system/src/view/DataTableHabitaciones.jsx
+++ booking-system/src/view/DataTableHabitaciones.jsx
@@ -1,128 +1,104 @@
-import { flexRender, getCoreRowModel, useReactTable, getPaginationRowModel } from '@tanstack/react-table'
-import { useState } from 'react'
-import { Pencil, Trash2, Info} from 'lucide-react'
-import classNames from 'classnames'
-import { dataHabitaciones } from '../utils/dataHabitaciones'
-import Modal from './modals/ModalHabitaciones'
+import { useState, useEffect, useCallback } from 'react';
+import Modal from './modals/ModalHabitaciones';
 
-export default function DataTableHabitaciones() {
-    const [data, setData] = useState(dataHabitaciones)
-    const [isModalOpen, setIsModalOpen] = useState(false)
+function DataTableCategorias() {
+  const [data, setData] = useState([])
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleClickOpenModal = () => {
-      setIsModalOpen(true)
+
+
+  {/*Listar habitaciones*/}
+  const fetchItems = useCallback(async () => {
+    try {
+      // Usando Thingproxy para evitar el problema de CORS
+      const proxyUrl = 'https://thingproxy.freeboard.io/fetch/';
+      const targetUrl = 'https://server-hotel-pi.vercel.app/rooms';
+      const response = await fetch(proxyUrl + targetUrl);
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
     }
+  }, []);
 
-    const handleAddProduct = (newProduct) => {
-      setData(prevData => [...prevData, newProduct])
+  useEffect(() => {
+    fetchItems(currentPage);
+  }, [currentPage, fetchItems]);
+
+  {/*Crear nueva habitacion*/}
+  const addOrder = async (newOrder) => {
+    const proxyUrl = 'https://thingproxy.freeboard.io/fetch/';
+    const targetUrl = 'https://server-hotel-pi.vercel.app/rooms/create';
+    const response = await fetch(proxyUrl + targetUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newOrder)
+    });
+  
+    if (response.ok) {
+      fetchItems(currentPage)
+      console.log('Habitacion añadida correctamente:', newOrder)
     }
+  };
 
-    const columns = [
-        {
-            accessorKey: '  ',
-            cell: () => (
-                <input type="checkbox" />
-            )
-        },
-        {
-            accessorKey: 'Nivel'
-        },
-        {
-            accessorKey: 'Nombre'
-        },
-        {
-            accessorKey: ' ',
-            cell: () => ( 
-                <>
-                    <button >
-                        <Pencil size={20} />
-                    </button>
-                    <button className='ml-14'>
-                        <Trash2 size={20} />
-                    </button>
-                    <button className='ml-14'>
-                        <Info size={20} />
-                    </button>
-                </>
-            )
-        },
-        
-    ]
-
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel()
+  {/*Eliminar ordenes*/}
+  const deleteOrder = async (id) => {
+    const response = await fetch(`https://backend-restaurante.deyvids.dev/api/v1/Category/Delete/${id}`, {
+      method: 'DELETE',
     })
+    if (response.ok) {
+      fetchItems(currentPage)
+    }
+  }
 
-    return (
-      <div className=' py-6 px-7 w-full flex flex-col rounded-lg overflow-hidden bg-white dark:bg-gray-700 dark:text-gray-200'>
-        <div>
-          <button className='bg-blue-600 text-white py-2 px-4 rounded-lg mb-2 hover:bg-blue-700' onClick={handleClickOpenModal}>Agregar Habitacion</button>
-          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onAddOrder={handleAddProduct} />
-        </div>
-          <table className='table-auto border border-gray-200 dark:border-gray-500 w-full ' >
-              <thead>
-                  {table.getHeaderGroups().map(headerGroup => (
-                      <tr key={headerGroup.id} className=' text-left'>
-                          {headerGroup.headers.map((header, index) => (
-                              <th key={header.id} className={`pl-3 py-4 ${index === 0 ? 'w-10' : ''}`} >
-                                  {header.isPlaceholder
-                                      ? null
-                                      : flexRender(
-                                          header.column.columnDef.header,
-                                          header.getContext()
-                                      )
-                                  }  
-                              </th>
-                          ))}
-                      </tr>
-                  ))}
-              </thead>
-              <tbody>
-                  {table.getRowModel().rows.map(row => (
-                      <tr key={row.id} className='border border-gray-200 dark:border-gray-500'>
-                          {row.getVisibleCells().map((cell, index) => (
-                              <td key={cell.id} className={`py-4 pl-3 ${index === row.getVisibleCells().length - 1  ? 'text-right pr-4 text-gray-500 dark:text-gray-200' : ''} `} >
-                                  {flexRender(
-                                      cell.column.columnDef.cell,
-                                      cell.getContext()
-                                  )}
-                              </td>
-                          ))}
-                      </tr>
-                  ))}
-              </tbody>
-          </table>
-          <div className='mt-4 flex items-center justify-center'>
-              <div className='flex items-center gap-2'>
-                  <button className='text-gray-600 dark:text-gray-300 py-1 px-2 rounded-lg 
-                  border border-white dark:border-none hover:bg-gray-100 dark:hover:bg-gray-500 disabled:hover:bg-white'
-                  onClick={() => table.previousPage()}
-                  disabled={!table.getCanPreviousPage()} > 
-                      {'<'}
-                  </button>
 
-                  {table.getPageOptions().map((value, key) => (
-                      <button key={key} 
-                      className={classNames({
-                          "text-gray-600 dark:text-gray-300 py-1 px-2 rounded-lg border border-white dark:border-none hover:bg-gray-100 dark:hover:bg-gray-500 disabled:hover:border-white": true,
-                          "bg-gray-200 dark:bg-gray-500 border border-gray-200": value === table.getState().pagination.pageIndex
-                      })}
-                      onClick={() => table.setPageIndex(value)}>
-                          {value + 1}
-                      </button>
-                  ))}
 
-                  <button className='text-gray-600 dark:text-gray-300 py-1 px-2 rounded-lg 
-                  border border-white dark:border-none hover:bg-gray-100 dark:hover:bg-gray-500 disabled:hover:bg-white'
-                  onClick={() => table.nextPage()}
-                  disabled={!table.getCanNextPage()}> 
-                      {'>'}
-                  </button>
-              </div>
-          </div>
+  const handleClickOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
+
+
+  return (
+    <div className=' py-6 px-7 w-full flex flex-col rounded-lg overflow-hidden bg-white dark:bg-gris-oscuro dark:text-gray-200'>
+      <div >
+        <button className='bg-gray-300 text-gray-500 font-semibold px-3 py-2' onClick={handleClickOpenModal} >Añadir Categoria</button>
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} onAddOrder={addOrder} />
       </div>
-    )
+      <table className='table-auto border border-gray-200 dark:border-gray-500 w-full'>
+        <thead>
+          <tr>
+            <th className="pl-3 py-4 text-left pr-4">ID</th>
+            <th className="pl-3 py-4 text-left pr-4">Codigo</th>
+            <th className="pl-3 py-4 text-left pr-4">Nombre</th>
+            <th className="pl-3 py-4 text-left pr-4"> Descripcion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex} className='border border-gray-200 dark:border-gray-500'>
+              {Object.entries(row).map(([, value], cellIndex) => {
+                const keyBase = `${rowIndex}-${cellIndex}`;
+                return <td key={keyBase} className="py-4 pl-3 pr-4 dark:text-gray-200">{value}</td>;
+              })}
+              <td className='w-64 '>
+                <div className='flex justify-between'>
+                  <button onClick={() => deleteOrder(row.id)} className='bg-gray-300 text-gray-500 font-semibold px-3 py-2'>Borrar</button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
+export default DataTableCategorias;
--- booking-system/src/view/modals/ModalHabitaciones.jsx
+++ booking-system/src/view/modals/ModalHabitaciones.jsx
@@ -3,30 +3,44 @@ import { useState } from 'react'
 /* eslint-disable react/prop-types */
 export default function Modal({ isOpen, onClose, onAddOrder}) {
   const [form, setForm] = useState({
-      Nombre: '',
-      Categoria: '',
-      Precio: '',
-      Detalles: '',
-      Nivel: ''
+      id: '',
+      roomNumber: '',
+      roomType: '',
+      price: '',
+      service: []
   });
 
   const handleChange = (e) => {
-      const value = e.target.name === 'Cantidad' && e.target.value !== '' ? Number(e.target.value) : e.target.value;
-      setForm({
-          ...form,
-          [e.target.name]: value,
-      });
-  };
+    let { name, value } = e.target;
+    if (name === 'price' && value !== '') {
+        value = Number(value);
+    } else if (name === 'service') {
+        const services = [...form.service];
+        if (e.target.checked) {
+            services.push(value);
+        } else {
+            const index = services.indexOf(value);
+            if (index > -1) {
+                services.splice(index, 1);
+            }
+        }
+        value = services;
+    }
+    setForm({
+        ...form,
+        [name]: value,
+    });
+};
 
   const handleSubmit = (e) => {
     e.preventDefault()
     onAddOrder(form)
     setForm({
-      Nombre: '',
-      Categoria: '',
-      Precio: '',
-      Detalles: '',
-      Nivel: ''
+      id: '',
+      roomNumber: '',
+      roomType: '',
+      price: '',
+      service: ''
     })
   }
 
@@ -39,8 +53,22 @@ export default function Modal({ isOpen, onClose, onAddOrder}) {
         <div className="modal-content bg-white rounded shadow-lg p-6 max-w-sm mx-auto">
             <h2 className="text-center mb-4 text-xl font-semibold dark:text-negro-claro">Añadir habitacion</h2>
             <form onSubmit={handleSubmit} className='dark:text-negro-claro'>
-                <input type="text" name="Nivel" value={form.Nivel || ''} onChange={handleChange} placeholder="Nivel" className="mb-2 p-2 w-full border rounded" />
-                <input type="text" name="Nombre" value={form.Nombre} onChange={handleChange} placeholder="Nombre" className="mb-2 p-2 w-full border rounded" />
+                <input type="number" name="id" value={form.id || ''} onChange={handleChange} placeholder="Id" className="mb-2 p-2 w-full border rounded" />
+                <input type="text" name="roomNumber" value={form.roomNumber || ''} onChange={handleChange} placeholder="Numero de habitación" className="mb-2 p-2 w-full border rounded" />
+                <select name="roomType" value={form.roomType} onChange={handleChange} className="mb-2 p-2 w-full border rounded dark:text-negro-claro">
+                    <option value="INDIVIDUAL">Individual</option>
+                    <option value="DOBLE">Doble</option>
+                    <option value="MATRIMONIAL">Matrimonial</option>
+                </select>
+                <input type="number" name="price" value={form.price} onChange={handleChange} placeholder="Precio" className="mb-2 p-2 w-full border rounded" />
+                <div className="mb-2">
+                    <label>Servicios:</label>
+                    <div>
+                        <input type="checkbox" name="service" value="WIFI" onChange={handleChange} /> WIFI
+                        <input type="checkbox" name="service" value="TV" onChange={handleChange} /> TV
+                        {/* Agrega más servicios según sea necesario */}
+                    </div>
+                </div>
                 <button type="submit" className="block w-full bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-4">Añadir</button>
             </form>
             <button onClick={onClose} className="block w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Cerrar</button>
